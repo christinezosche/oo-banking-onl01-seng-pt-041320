edd46f51c1e6cfe202030863e784fdefa04cada6
Christine Zosche <christine.zosche@gmail.com>
2020-05-13 17:24:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? == false
+    if @sender.status == "closed"
       "Transaction rejected. Please check your account balance."
       @status = "rejected"
     else
