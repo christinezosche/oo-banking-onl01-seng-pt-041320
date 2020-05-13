676b79905bf2bf0349ce32b5db46ebe0c2607ac8
Christine Zosche <christine.zosche@gmail.com>
2020-05-13 17:32:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? && @sender.balance >= @amount && @status != "complete"
+    if valid? && @sender.balance >= @amount && @status != "complete"
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
