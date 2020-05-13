c8a3d7133978c0de1974a3cdb610ff6c6afb80f6
Christine Zosche <christine.zosche@gmail.com>
2020-05-13 17:33:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,5 +23,16 @@ class Transfer
     end
   end
     
-    
+  def reverse_transfer
+    if @status = "complete"
+      if valid? && @sender.balance >= @amount && @status != "complete"
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+      
 end
